596585ed735ff7086d2b0b0627c9f49386b4e4a1
logan1x <sharmakhushal78@gmail.com>
2022-12-07 23:08:19 +0530

    [feat]: toast for isLoggedin info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { HiOutlineUserGroup } from "react-icons/hi";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { TbArrowsJoin } from "react-icons/tb";
 import heroimg from "../assets/heroimg.png";
 
 function Home() {
+  const handleClick = () => {
+    // TODO: add logic to check if user is logged in or not
+    toast.error("You have not logged-in yet!");
+    // TODO: if not loggedIn, add logic to redirect to login page or show a page where user wanted to go.
+  };
   return (
     <div className="flex flex-col gap-8 items-center justify-center grow text-xl">
       <div className="flex justify-center items-center flex-col">
@@ -15,18 +21,36 @@ function Home() {
         </p>
       </div>
       <div className="flex md:flex-row flex-col gap-2">
-        <button className="transition delay-150 flex justify-items-start items-center gap-2 px-8 py-4 bg-amber-200 group/item hover:bg-amber-300 rounded">
+        <button
+          className="transition delay-150 flex justify-items-start items-center gap-2 px-8 py-4 bg-amber-200 group/item hover:bg-amber-300 rounded"
+          onClick={() => {
+            handleClick();
+          }}
+        >
           <HiOutlineUserGroup className="transition delay-150 text-3xl p-1 bg-amber-300 group-hover/item:bg-amber-400 rounded" />
           My groups
         </button>
-        <button className="transition delay-150 flex items-center gap-2 px-8 py-4 bg-green-200 rounded group/item hover:bg-green-300 rounded">
+        <button
+          className="transition delay-150 flex items-center gap-2 px-8 py-4 bg-green-200 rounded group/item hover:bg-green-300 rounded"
+          onClick={() => {
+            handleClick();
+          }}
+        >
           <AiOutlineUsergroupAdd className="transition delay-150 text-3xl p-1 bg-green-300 group-hover/item:bg-green-400 rounded" />
           Create a group
         </button>
-        <button className="transition delay-150 flex items-center gap-2 px-8 py-4 bg-red-200 rounded group/item hover:bg-red-300">
+        <button
+          className="transition delay-150 flex items-center gap-2 px-8 py-4 bg-red-200 rounded group/item hover:bg-red-300"
+          onClick={() => {
+            handleClick();
+          }}
+        >
           <TbArrowsJoin className="transition delay-150 text-3xl p-1 bg-red-300 group-hover/item:bg-red-400 rounded" />
           Join a group
         </button>
+      </div>
+      <div>
+        <Toaster position="bottom-right" reverseOrder={false} />
       </div>
     </div>
   );
