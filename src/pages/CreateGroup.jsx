b4c08a90117438665d05ef739f08f3ad15cd75bc
parthpandyappp <parthpandyappp@gmail.com>
2022-12-10 15:09:34 +0530

    [feat]: creation of group

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { createSecretGroup } from "../features";
 
 function CreateGroup() {
-  return <div>CreateGroup</div>;
+  const [groupName, setGroupName] = useState("");
+  const dispatch = useDispatch();
+  const { authUser } = useSelector((state) => state.auth);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const groupData = {
+      gid: uuidv4(),
+      creator: authUser.uid,
+      createdAt: new Date(),
+      groupName: groupName,
+      members: [],
+    };
+    dispatch(
+      createSecretGroup({
+        groupData,
+      })
+    );
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setGroupName(e.target.value)} />
+        <button type="submit">Create group</button>
+      </form>
+    </div>
+  );
 }
 
 export { CreateGroup };
