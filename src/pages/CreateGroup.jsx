95de62ee31d8a493ab745bfa49b35b76d1d40de3
parthpandyappp <parthpandyappp@gmail.com>
2022-12-10 15:42:15 +0530

    [design]: basic group creation

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -23,10 +23,23 @@ function CreateGroup() {
     );
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setGroupName(e.target.value)} />
-        <button type="submit">Create group</button>
+    <div className="w-1/2 mx-auto flex flex-col justify-center items-center -mt-24 h-screen">
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <label htmlFor="gname" className="text-sm text-gray-500 font-bold">
+          Group name
+        </label>
+        <input
+          type="text"
+          name="gname"
+          onChange={(e) => setGroupName(e.target.value)}
+          className="px-5 py-2 border border-2 rounded"
+        />
+        <button
+          type="submit"
+          className="transition delay-150 font-semibold text-gray-700 px-8 py-1 bg-amber-200 group/item hover:bg-amber-300 rounded w-4/5 text-center text-lg self-center mt-2"
+        >
+          Create group
+        </button>
       </form>
     </div>
   );
